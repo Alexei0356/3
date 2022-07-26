e2453d3bf6199ccf5078e575ffd59b6ed495ec13
Alexei0356 <69077245+Alexei0356@users.noreply.github.com>
2022-07-26 22:59:20 +0300

    Update a.php

--- a.php
+++ a.php
@@ -11,14 +11,18 @@ require __DIR__ . '/vendor/autoload.php';
 
 
 //Проверка выражения на содержание лишних символов
-preg_match("/^[\+\-\*\/\%\d\(\)]+$/",$_POST['data']) ;
-
-//Условие, в котором производится проверка на совпадение скобок.Если они совпали, то производится вычисление и выводится результат, если нет- сообщение об ошибке
-if(substr_count($_POST['data'], "(") != substr_count($_POST['data'], ")")) {
-    echo 'Скобки не совпадают';
-} else {
-    $evaluator = new \Matex\Evaluator();
-    echo $evaluator->execute($_POST['data']);
+if (preg_match("/^[\+\-\*\/\%\d\(\)]+$/",$_POST['data'])) {
+    if(substr_count($_POST['data'], "(") != substr_count($_POST['data'], ")")) {
+        echo 'Скобки не совпадают';
+    } else {
+        $evaluator = new \Matex\Evaluator();
+        echo $evaluator->execute($_POST['data']);
+    }
+} else{
+    echo "Выражение введено с ошибкой";
 
 }
+
+//Условие, в котором производится проверка на совпадение скобок.Если они совпали, то производится вычисление и выводится результат, если нет- сообщение об ошибке
+
 ?>
